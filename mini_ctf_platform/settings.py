16b9a5375c527d3ce2e57f36ab18352333934809
AnOnyme77 <anonyme77@protonmail.com>
2018-04-10 00:29:52 +0200

    debug

--- mini_ctf_platform/settings.py
+++ mini_ctf_platform/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '1el1%8b6h38u504*hp1s)eg#wa!2xsk7)qdc8*svl(#2$^%2p4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
