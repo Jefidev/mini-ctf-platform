fe3692ea5bdab952564794779019dee637da0ebe
AnOnyme77 <anonyme77@protonmail.com>
2018-04-10 00:38:26 +0200

    debug

--- ctf_platform/urls.py
+++ ctf_platform/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     re_path(r'^$', views.index, name='index'),
     re_path(r'scoreboard', views.scoreboard, name='scoreboard'),
     re_path(r'challenge/(?P<challenge_id>\d)$', views.challenge, name='challenge'),
-    re_path(r'team/(?P<team_id>\d)$', views.team, name='team'),
+    re_path(r'team/(?P<team_id>\d)$', views.team, name='team')
 ]
