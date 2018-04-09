69ec063812d1af7c1d533e1a2d69e9e7bf775217
AnOnyme77 <anonyme77@protonmail.com>
2018-04-09 23:50:22 +0200

    Fix links in challenge page. resolve #3

--- ctf_platform/urls.py
+++ ctf_platform/urls.py
@@ -2,6 +2,6 @@ from django.urls import re_path
 from . import views
 
 urlpatterns = [
-    re_path(r'^$', views.index),
+    re_path(r'^$', views.index, name='index'),
     re_path(r'challenge/(?P<challenge_id>\d)$', views.challenge, name='challenge'),
 ]
